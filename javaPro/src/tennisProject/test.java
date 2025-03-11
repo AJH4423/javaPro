698e0e47156c54cf2dbf34b30aa485fb8759a2c9
AJH <anjunho12@naver.com>
2025-03-11 17:33:03 +0900

    commit tennisproject

---
+++ javaPro/src/tennisProject/test.java
@@ -0,0 +1,19 @@
+package tennisProject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> male = new ArrayList<String>();
+		male.add("양재원");
+		male.add("안준호");
+		male.add("이진욱");
+		int index = 0;
+		System.out.println(male.get(index));
+		
+	} // main
+
+} // class
